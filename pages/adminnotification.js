de89fe203618ddfe588180db8ede6c4fd757d1a0
harpreet kaur <Harpreet-kaurr>
2022-08-31 19:00:48 +0530

    noti added

---
+++ pages/adminnotification.js
@@ -0,0 +1,21 @@
+import { Fragment,useEffect } from "react";
+import Notification from "../modules/Admin Panel/Notification";
+import {useRouter} from 'next/router'
+import { getAdminOnBoardFromCookie } from "../auth/userCookies";
+export default function Home() {
+    const router = useRouter();
+    var JWTtoken = getAdminOnBoardFromCookie();
+    useEffect(()=>{
+        if(!JWTtoken){
+            router.push("/adminlogin")
+        }
+        else{
+            router.push("/admindashboard")
+        }
+    },[])
+    return (
+        <Fragment>  
+            <Notification></Notification>
+        </Fragment>
+    );
+}
