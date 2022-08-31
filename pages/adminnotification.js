6dfabcd71d22f9b01dda333dcb7ddbd53d32d7f5
harpreet kaur <Harpreet-kaurr>
2022-08-31 19:11:38 +0530

    new noti added

--- pages/adminnotification.js
+++ pages/adminnotification.js
@@ -10,7 +10,7 @@ export default function Home() {
             router.push("/adminlogin")
         }
         else{
-            router.push("/admindashboard")
+            router.push("/adminnotification")
         }
     },[])
     return (
