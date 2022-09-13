0e77905ffa2608b1fb1f12e480d2016a9e31daf1
harpreet kaur <Harpreet-kaurr>
2022-09-13 15:55:10 +0530

    added

--- modules/Filter.js
+++ modules/Filter.js
@@ -3,8 +3,12 @@ import style from './css/Filter.module.css'
 
 const Filter = () => {
     const [toggle, setToggle] = useState(false);
+    const [sort,setSort] = useState(false)
     const handleClick = () =>{
         setToggle(!toggle);
+    }
+    const sortHandler = () =>{
+        setSort(prev=>!prev);
     }
     return (
         <div className={`p-relative d-flex d-align-center d-justify-end gap-3 mt-40 mb-32 ${style["filter-margin"]}`}>
@@ -19,10 +23,39 @@ const Filter = () => {
                     <a className={`d-block text-black f-500 font-16 l-134 ${style["filters-heading-item-wrapper"]}`} href="#">Recently Sold</a>
                 </div>
             </div>
-            <div className={`rounded-8 bg-pink mb-8 d-flex d-align-center d-justify-space-between  ${style["sort-heading-wrapper"]}`}>
-                <a className='f-500 font-16 l-134'>Sort</a>
-                <img src='images/arrow-down.png'></img>
+            <div className={`p-absolute `}>
+                <div className={`rounded-8 bg-pink mb-8 d-flex d-align-center d-justify-space-between  ${style["sort-heading-wrapper"]}`}>
+                    <a className='f-500 font-16 l-134'>Sort</a>
+                    <img onClick={sortHandler} src='images/arrow-down.png'></img>
+                </div>
+                <div className={`p-absolute rounded-8 bg-pink ${style["sort-position-absolute"]} ${sort ? "d-block" : "d-none"}`}>
+                    <div className={`d-flex d-align-center d-justify-space-between ${style["sort-col-1"]}`}>
+                        <h6 className='f-500 l-137'>Price</h6>
+                        <h6 className='font-13 f-400 l-137 text-very-light-gray'>Clear</h6>
+                    </div>
+                    <div className={`d-flex d-flex-column bg-active gap-1 ${style["sort-col-2"]}`}>
+                        <div className='bg-pink rounded-8 d-flex d-align-center d-justify-space-around'>
+                            <h6 className='f-400 l-137'>Great Britain Pound(GBP)</h6>
+                            <img src='images/arrow-down.png'></img>
+                        </div>
+                        <div className='d-flex d-align-center d-justify-space-between'>
+                            <div className={`bg-pink rounded-8 d-flex ${style["min-wrapper"]}`}>
+                                <h6 className='f-400 l-137'>Min</h6>
+                            </div>
+                            <div className={`d-flex `}>
+                                <h6 className='f-400 l-137'>to</h6>
+                            </div>
+                            <div className={`bg-pink rounded-8 d-flex ${style["max-wrapper"]}`}>
+                                <h6 className='f-400 l-137'>Min</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`d-flex d-justify-center ${style["btn-continue"]}`}>
+                        <button className='rounded-8 b-none font-20 f-500 l-137'>Continue</button>
+                    </div>
+                </div>
             </div>
+            
         </div>
     )
 }
